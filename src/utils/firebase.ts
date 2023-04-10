028a7104c3f7fb7bffcfd664414a49bebbd8ea7e
Mark <105296060+bmark210@users.noreply.github.com>
2023-04-10 19:41:22 +0300

    Update and rename firebase.tsx to firebase.ts

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -3,12 +3,8 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyCnLWuFAUA7ahql5F64cPmnojlNjV7xWeM",
-  authDomain: "instagram-clone-e904f.firebaseapp.com",
-  projectId: "instagram-clone-e904f",
-  storageBucket: "instagram-clone-e904f.appspot.com",
-  messagingSenderId: "522313280912",
-  appId: "1:522313280912:web:c44e1c273b73eadd251715"
+  // your config
+
 };
 const firebase = initializeApp(config);
 const firestore = getFirestore();
